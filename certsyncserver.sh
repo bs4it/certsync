b85ce7ca8f073325a16aa9b7cdd5121ef9d92a6d
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-03-30 06:57:00 -0300

    modified:   certsyncserver.sh, mkdir

--- certsyncserver.sh
+++ certsyncserver.sh
@@ -8,6 +8,7 @@ do
   tgt_sha256=$(sha256sum /home/certsync/certs/$domain/privkey.pem | awk '{print $1}')
   if ! [ $tgt_sha256 == $src_sha256 ]; then
     echo "Cert has changed."
+    mkdir -p /home/certsync/certs/$domain
     cp /etc/letsencrypt/live/$domain/privkey.pem /home/certsync/certs/$domain/privkey.pem
     cp /etc/letsencrypt/live/$domain/fullchain.pem /home/certsync/certs/$domain/fullchain.pem
     openssl pkcs12 -export -in /home/certsync/certs/$domain/fullchain.pem -inkey /home/certsync/certs/$domain/privkey.pem -out /home/certsync/certs/$domain/cert_combined.pfx -password pass:ju5u6hxi

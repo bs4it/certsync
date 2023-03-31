39a5b4f09ee334ebf859bd679330cafbdee6cadf
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-03-31 17:24:19 -0300

    modified:   certsyncserver.sh

--- certsyncserver.sh
+++ certsyncserver.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-domains=$(dirname "$0")
+if ! [ -f /opt/bs4it/certsync/domains.conf ]; then
+  echo "File '/opt/bs4it/certsync/domains.conf' not found. Creating it."
+  touch /opt/bs4it/certsync/domains.conf
+fi
+# read domain list from file
+read -d $'\x04' domains < /opt/bs4it/certsync/domains.conf
 
 for domain in $domains;
 do
   src_sha256=$(sha256sum /etc/letsencrypt/live/$domain/privkey.pem | awk '{print $1}')
   tgt_sha256=$(sha256sum /home/certsync/certs/$domain/privkey.pem | awk '{print $1}')
-  if ! [ $tgt_sha256 -eq $src_sha256 ]; then
-    echo "Cert has changed."
+  if ! [ $tgt_sha256 == $src_sha256 ]; then
+    echo "Cert for $domain has changed."
     mkdir -p /home/certsync/certs/$domain
     cp /etc/letsencrypt/live/$domain/privkey.pem /home/certsync/certs/$domain/privkey.pem
     cp /etc/letsencrypt/live/$domain/fullchain.pem /home/certsync/certs/$domain/fullchain.pem
@@ -15,6 +20,6 @@ do
     sha256sum /home/certsync/certs/$domain/privkey.pem | awk '{print $1}' > /home/certsync/certs/$domain/sha256sum
     chown -R certsync:certsync /home/certsync/certs/$domain/*
   else
-    echo "Cert is the same, quitting"
+    echo "Cert for $domain is the same, quitting"
   fi
 done

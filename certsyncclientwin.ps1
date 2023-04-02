501e74f1a13f9496222b02ce3ac8b485325ba74f
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-04-02 01:12:00 -0300

    modified:   certsyncclientwin.ps1

--- certsyncclientwin.ps1
+++ certsyncclientwin.ps1
@@ -1,3 +1,4 @@
+
 $services="haproxy.service sshd.service"
 $server="172.24.0.26"
 $user="certsync"
@@ -10,3 +11,10 @@ if ( -Not (Test-Path -Path $scriptPath\domains.conf -PathType Leaf )) {
 }
 
 $domains = Get-Content $scriptPath\domains.conf
+
+$domains | ForEach-Object {
+    $remotehome = ssh -i c:\opt\bs4it\ssh\certsync_id_ed25519 $user@$server printenv HOME
+    $src_sha256 = ssh -i c:\opt\bs4it\ssh\certsync_id_ed25519 $user@$server cat $remotehome/certs/$domain/sha256sum
+    $tgt_sha256 = ((Get-FileHash -Algorithm SHA256 -Path $scriptPath\..\certs\$_\cert_combined.pfx).Hash).Tolower()
+    Write-Host $tgt_sha256
+}

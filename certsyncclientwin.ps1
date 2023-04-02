c8939bcc7757d276abc13de284f3f879994ee335
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-04-02 02:58:02 -0300

    modified:   certsyncclientwin.ps1

--- certsyncclientwin.ps1
+++ certsyncclientwin.ps1
@@ -11,11 +11,25 @@ if ( -Not (Test-Path -Path $scriptPath\domains.conf -PathType Leaf )) {
 }
 
 $domains = Get-Content $scriptPath\domains.conf
-
+$certchanged = 0
 $domains | ForEach-Object {
     $remotehome = ssh -i c:\opt\bs4it\ssh\certsync_id_ed25519 $user@$server printenv HOME
     $src_sha256 = ssh -i c:\opt\bs4it\ssh\certsync_id_ed25519 $user@$server sha256sum $remotehome/certs/$_/cert_combined.pfx
     $tgt_sha256 = ((Get-FileHash -Algorithm SHA256 -Path $scriptPath\..\certs\$_\cert_combined.pfx).Hash).Tolower()
-    Write-Host $src_sha256
-    Write-Host $tgt_sha256
+    if ( $tgt_sha256 -ne $src_sha256 ) {
+        Write-Host "Cert for $domain has changed."
+        $certchanged = 1
+        New-Item -ItemType Directory $scriptPath\..\certs\$_
+        scp certsync@$server:$remotehome/certs/$domain/cert_combined.pfx $scriptPath\..\certs\$_\cert_combined.pfx
+    }
 }
+
+if ( $certchanged -eq 1 ) {
+    Write-Host "Deploy Cerificate..."
+    for service in $services;
+    do
+    echo Issuing restart command to $service
+    /usr/bin/systemctl restart $service
+    done
+}
+  

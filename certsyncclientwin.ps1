34769028b4c2aa4d11cf80533bc9e5addb55ad33
unknown <fernando.dellatorre@bs4it.com.br>
2023-09-16 21:42:09 -0300

    Add cert to IIS

--- certsyncclientwin.ps1
+++ certsyncclientwin.ps1
@@ -1,6 +1,6 @@
 
 $services="haproxy.service sshd.service"
-$server="172.24.0.26"
+$server="172.24.0.3"
 $user="certsync"
 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
@@ -12,9 +12,11 @@ if ( -Not (Test-Path -Path $scriptPath\domains.conf -PathType Leaf )) {
 
 $domains = Get-Content $scriptPath\domains.conf
 $certchanged = 0
+Import-Module Webadministration
 $domains | ForEach-Object {
+    $domain = $_
     $remotehome = ssh -i $scriptPath\..\ssh\certsync_id_ed25519 $user@$server printenv HOME
-    $src_sha256 = ssh -i $scriptPath\..\ssh\certsync_id_ed25519 $user@$server sha256sum $remotehome/certs/$_/cert_combined.pfx
+    $src_sha256 = ssh -i $scriptPath\..\ssh\certsync_id_ed25519 $user@$server "sha256sum $remotehome/certs/$_/cert_combined.pfx | cut -d ' ' -f 1"
     $tgt_sha256 = Get-FileHash -ErrorAction SilentlyContinue -Algorithm SHA256 -Path $scriptPath\..\certs\$_\cert_combined.pfx
     if ( $tgt_sha256 -eq $null ){
         $tgt_sha256 = 1
@@ -25,16 +27,16 @@ $domains | ForEach-Object {
     if ( $tgt_sha256 -ne $src_sha256 ) {
         Write-Host "Cert for $_ has changed."
         $certchanged = 1
-        New-Item -Force -ItemType Directory $scriptPath\..\certs\$_
+        New-Item -Force -ItemType Directory $scriptPath\..\certs\$_ | Out-Null
         scp -i $scriptPath\..\ssh\certsync_id_ed25519 certsync@${server}:${remotehome}/certs/$_/cert_combined.pfx $scriptPath\..\certs\$_\cert_combined.pfx
+        Write-Host "Removing old cert for " $domain
+        Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Subject -match $domain } | Remove-Item
+        Write-Host "Deploying new cert for " $domain
+        $NewCert = Import-PfxCertificate -FilePath $scriptPath\..\certs\$_\cert_combined.pfx -Password ( ConvertTo-SecureString "imp0rtp455" -AsPlainText -Force ) -CertStoreLocation Cert:\LocalMachine\My
+        $site = Get-ChildItem -Path "IIS:\Sites" | Where-Object {( $_.Name -eq "Veeam Availability Console Web UI" )}
+        $binding = $site.Bindings.Collection | Where-Object {( $_.protocol -eq 'https' -and $_.bindingInformation -match ':443:')}
+        $binding.AddSslCertificate($NewCert.Thumbprint, "my")
+
+
     }
 }
-
-# if ( $certchanged -eq 1 ) {
-#     Write-Host "Deploy Cerificate..."
-#     for service in $services;
-#     do
-#     echo Issuing restart command to $service
-#     /usr/bin/systemctl restart $service
-#     done
-# }
